6f4de10a85cbbed1b9edb4f36e493acccf6cb5f5
AlexWeiDev <alexwei.work@gmail.com>
2017-05-10 11:35:44 -0700

    Add files via upload

---
+++ Website-CHRON watch/js/sidebar.js
@@ -0,0 +1,48 @@
+                  function openNav() { 
+                      document.getElementById("mySidenav").style.width = "100%"; 
+                      // document.getElementById("main").style.marginLeft = "250px"; 
+                      document.body.style.backgroundColor = "rgba(0,0,0,0.5)"; 
+                  } 
+
+                   function closeNav() { 
+                      document.getElementById("mySidenav").style.width = "0"; 
+                      // document.getElementById("main").style.marginLeft= "0"; 
+                      document.body.style.backgroundColor = "white"; 
+                  } 
+
+
+                  function openNav() { 
+                      document.getElementById("mySidenav").style.width = "100%"; 
+                      // document.getElementById("main").style.marginLeft = "250px"; 
+                      document.body.style.backgroundColor = "rgba(0,0,0,0.5)"; 
+                  } 
+
+                   function closeNav() { 
+                      document.getElementById("mySidenav").style.width = "0"; 
+                      // document.getElementById("main").style.marginLeft= "0"; 
+                      document.body.style.backgroundColor = "white"; 
+                  } 
+                  
+                  function openCart() { 
+                      document.getElementById("myCart").style.width = "100%";  // width of the cart side bar
+                      // document.getElementById("main").style.marginLeft = "250px"; 
+                      document.body.style.backgroundColor = "rgba(0,0,0,0.4)"; 
+                  } 
+         
+                  function closeCart() { 
+                      document.getElementById("myCart").style.width = "0"; 
+                      // document.getElementById("main").style.marginLeft= "0"; 
+                      document.body.style.backgroundColor = "white"; 
+                  } 
+                     function openSearch() { 
+                            document.getElementById("mySearch").style.width = "100%";  //width of the searcb side bar
+
+                            // document.getElementById("main").style.marginLeft = "250px"; 
+                            document.body.style.backgroundColor = "rgba(0,0,0,0.4)"; 
+                        } 
+               
+                        function closeSearch() { 
+                            document.getElementById("mySearch").style.width = "0"; 
+                            // document.getElementById("main").style.marginLeft= "0"; 
+                            document.body.style.backgroundColor = "white"; 
+                        } 
